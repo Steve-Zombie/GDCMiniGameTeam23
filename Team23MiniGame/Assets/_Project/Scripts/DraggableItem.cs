97c848382967f15047ba97698fb1748e3662b63b
redbeluga <kevinxu9455@gmail.com>
2025-09-29 13:57:07 -0400

    Add draggable item implementation

--- Team23MiniGame/Assets/_Project/Scripts/DraggableItem.cs
+++ Team23MiniGame/Assets/_Project/Scripts/DraggableItem.cs
@@ -2,36 +2,53 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [Header("References")]
+    [SerializeField] private Image image;
     [SerializeField] private Item item;
 
-    private Canvas canvas;
-    private Image dragGhost;
+    private Canvas _rootCanvas;
+    private Image _dragGhost;
+
+    /// public access of item data
+    public Item Item => item;
+
+    void OnValidate()
+    {
+        if (item != null)
+            image.sprite = item.sprite;
+    }
+
+    void Awake()
+    {
+        _rootCanvas = GetComponentInParent<Canvas>();
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        dragGhost = new GameObject("Drag Ghost", typeof(Image)).GetComponent<Image>();
-        dragGhost.transform.SetParent(canvas.transform, false);
-        dragGhost.sprite = GetComponent<Image>().sprite;
-        dragGhost.raycastTarget = false;
+        _dragGhost = new GameObject("Drag Ghost", typeof(Image)).GetComponent<Image>();
+        _dragGhost.transform.SetParent(_rootCanvas.transform, false);
+        _dragGhost.rectTransform.sizeDelta = GetComponent<RectTransform>().sizeDelta;
+        _dragGhost.sprite = GetComponent<Image>().sprite;
+        _dragGhost.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            canvas.transform as RectTransform,
+            _rootCanvas.transform as RectTransform,
             eventData.position,
             eventData.pressEventCamera,
             out var localPos
         );
-        dragGhost.rectTransform.anchoredPosition = localPos;
+        _dragGhost.rectTransform.anchoredPosition = localPos;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (dragGhost != null)
-            Destroy(dragGhost.gameObject);
+        if (_dragGhost != null)
+            Destroy(_dragGhost.gameObject);
     }
 }
